386796a2dc3d7b1a7848400f06ceb5b7139f1428
Amr Hassan <amr.hassan@gmail.com>
2017-12-15 17:42:00 +0100

    0.3.1

--- nakacli.rb
+++ nakacli.rb
@@ -1,8 +1,8 @@
 class Nakacli < Formula
   desc "CLI Client for Nakadi"
   homepage "https://github.com/amrhassan/nakacli"
-  url "https://github.com/amrhassan/nakacli/releases/download/0.3.0/nakacli-0.3.0-x86_64-apple-darwin.tar.bz2"
-  sha256 "150037bd2d5297b562f4a8824f8d8711fdda9f05a0ff839e229a90b89ebed7fb"
+  url "https://github.com/amrhassan/nakacli/releases/download/0.3.1/nakacli-0.3.1-x86_64-apple-darwin.tar.bz2"
+  sha256 "d1f90a0ea93fa17480902c82c3a9b244d0dccb9777e16750ee567ba5dc82fe7f"
 
   bottle :unneeded
 
